2955a5f35839f9071a854f13919db38c470ab81d
Emile Bangma <github@emilebangma.com>
2025-03-24 06:03:18 +0100

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,3 +1,11 @@
 
-# Add `@use "./themes";` import to custom.scss
-sed -ir 's#@use "./base.scss";#@use "./base.scss";\n@use "./themes";#' custom.scss
+echo "Verifying setup..."
+if grep -q '^@use "./themes";' custom.scss
+then
+  # Import already present in custom.scss
+  echo "Import line already present in custom.scss. Skipping..."
+else
+  # Add `@use "./themes";` import to custom.scss
+  sed -ir 's#@use "./base.scss";#@use "./base.scss";\n@use "./themes";#' custom.scss
+  echo "Added import line to custom.scss...
+fi
