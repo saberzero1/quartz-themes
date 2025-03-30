d7faf87b73997740e9b094e6069b0ef6fa6ad6e2
Emile Bangma <github@emilebangma.com>
2025-03-30 13:14:21 +0200

    Update action.sh

--- action.sh
+++ action.sh
@@ -140,7 +140,7 @@ else
   echo_warn "_dark.scss missing"
 fi
 
-if [ check_file "$THEME_DIR/_light.scss" ]; then
+if [ check_file "$THEME_DIR/_light.scss" -eq 0 ]; then
   echo "$THEME_DIR/_light.scss"
   echo check_file "$THEME_DIR/_light.scss"
   echo [ check_file "$THEME_DIR/_light.scss" ]
