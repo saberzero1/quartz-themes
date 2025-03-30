51758de9524d0915a74c6d7e404544783dc799e6
Emile Bangma <github@emilebangma.com>
2025-03-30 12:52:20 +0200

    Update action.sh

--- action.sh
+++ action.sh
@@ -15,7 +15,7 @@ echo_warn() { echo -e "${YELLOW}$1${NC}"; }
 echo_ok() { echo -e "${GREEN}$1${NC}"; }
 echo_info() { echo -e "${BLUE}$1${NC}"; }
 
-check_file() { echo ( -e "$1" -eq 0 ); }
+check_file() { echo ( [ -e "$1" ] -eq 0 ); }
 
 THEME_DIR="themes"
 QUARTZ_STYLES_DIR="quartz/styles"
