87263469796243610fbbb546a36c57ef01d9e546
Emile Bangma <github@emilebangma.com>
2025-03-30 12:38:10 +0200

    Update action.sh

--- action.sh
+++ action.sh
@@ -18,14 +18,14 @@ echo_info() { echo -e "${BLUE}$1${NC}"; }
 THEME_DIR="themes"
 QUARTZ_STYLES_DIR="quartz/styles"
 
-if [ -f "./${QUARTZ_STYLES_DIR}/custom.scss" ]; then
+if [ -f "./$QUARTZ_STYLES_DIR/custom.scss" ]; then
   echo_ok "Quartz root succesfully detected..."
-  THEME_DIR="./${QUARTZ_STYLES_DIR}/${THEME_DIR}"
+  THEME_DIR="./$QUARTZ_STYLES_DIR/$THEME_DIR"
 else
   echo_warn "Quartz root not detected, checking if we are in the styles directory..."
   if [ -f "./custom.scss" ]; then
     echo_ok "Styles directory detected..."
-    THEME_DIR="./${THEME_DIR}"
+    THEME_DIR="./$THEME_DIR"
   else
     echo_err "Cannot detect Quartz repository. Are you in the correct working directory?" 1>&2
     exit 1
@@ -59,18 +59,18 @@ echo -e "Theme ${BLUE}$*${NC} parsed to $(echo_info ${THEME})"
 
 echo "Validating theme..."
 
-GITHUB_URL_BASE="https://raw.githubusercontent.com/saberzero1/quartz-themes/master/"
-GITHUB_OUTPUT_DIR="themes/"
-GITHUB_OVERRIDE_DIR="extras/themes/"
-GITHUB_EXTRAS_DIR="extras/"
-GITHUB_THEME_DIR="${THEME}/"
-CSS_INDEX_URL="${GITHUB_URL_BASE}${GITHUB_OUTPUT_DIR}${GITHUB_THEME_DIR}_index.scss"
-CSS_FONT_URL="${GITHUB_URL_BASE}${GITHUB_OUTPUT_DIR}${GITHUB_THEME_DIR}_fonts.scss"
-CSS_DARK_URL="${GITHUB_URL_BASE}${GITHUB_OUTPUT_DIR}${GITHUB_THEME_DIR}_dark.scss"
-CSS_LIGHT_URL="${GITHUB_URL_BASE}${GITHUB_OUTPUT_DIR}${GITHUB_THEME_DIR}_light.scss"
-CSS_OVERRIDE_URL="${GITHUB_URL_BASE}${GITHUB_OVERRIDE_DIR}${GITHUB_THEME_DIR}_index.scss"
-CSS_EXTRAS_URL="${GITHUB_URL_BASE}${GITHUB_EXTRAS_DIR}"
-README_URL="${GITHUB_URL_BASE}${GITHUB_OVERRIDE_DIR}${GITHUB_THEME_DIR}README.md"
+GITHUB_URL_BASE="https://raw.githubusercontent.com/saberzero1/quartz-themes/master"
+GITHUB_OUTPUT_DIR="themes"
+GITHUB_OVERRIDE_DIR="extras/themes"
+GITHUB_EXTRAS_DIR="extras"
+GITHUB_THEME_DIR="$THEME"
+CSS_INDEX_URL="${GITHUB_URL_BASE}/${GITHUB_OUTPUT_DIR}/${GITHUB_THEME_DIR}/_index.scss"
+CSS_FONT_URL="${GITHUB_URL_BASE}/${GITHUB_OUTPUT_DIR}/${GITHUB_THEME_DIR}/_fonts.scss"
+CSS_DARK_URL="${GITHUB_URL_BASE}/${GITHUB_OUTPUT_DIR}/${GITHUB_THEME_DIR}/_dark.scss"
+CSS_LIGHT_URL="${GITHUB_URL_BASE}/${GITHUB_OUTPUT_DIR}/${GITHUB_THEME_DIR}/_light.scss"
+CSS_OVERRIDE_URL="${GITHUB_URL_BASE}/${GITHUB_OVERRIDE_DIR}/${GITHUB_THEME_DIR}/_index.scss"
+CSS_EXTRAS_URL="${GITHUB_URL_BASE}/${GITHUB_EXTRAS_DIR}"
+README_URL="${GITHUB_URL_BASE}/${GITHUB_OVERRIDE_DIR}/${GITHUB_THEME_DIR}/README.md"
 
 PULSE=$(curl -o /dev/null --silent -lw '%{http_code}' "${CSS_INDEX_URL}")
 
@@ -104,52 +104,52 @@ curl -s -S -o ${THEME_DIR}/extras/_index.scss "${CSS_OVERRIDE_URL}"
 
 echo "Fetching extras..."
 
-curl -s -S -o ${THEME_DIR}/extras/hide-toggle.scss "${CSS_EXTRAS_URL}hide-toggle.scss"
+curl -s -S -o ${THEME_DIR}/extras/hide-toggle.scss "${CSS_EXTRAS_URL}/hide-toggle.scss"
 
 echo "Fetching README file..."
 
-curl -s -S -o ${THEME_DIR}/README.md "${README_URL}"
+curl -s -S -o ${THEME_DIR}/README.md "$README_URL"
 
 echo "Checking theme files..."
 
-if [ -f "${THEME_DIR}/_index.scss" ]; then
+if [ -f "$THEME_DIR/_index.scss" ]; then
   echo_ok "_index.scss exists"
 else
   echo_err "_index.scss missing" 1>&2
   exit 1
 fi
 
-if [ -f "${THEME_DIR}/_fonts.scss" ]; then
+if [ -f "$THEME_DIR/_fonts.scss" ]; then
   echo_ok "_fonts.scss exists"
 else
   echo_err "_fonts.scss missing" 1>&2
   exit 1
 fi
 
-if [ -f "${THEME_DIR}/_dark.scss" ]; then
+if [ -f "$THEME_DIR/_dark.scss" ]; then
   echo_ok "_dark.scss exists"
 else
   echo_warn "_dark.scss missing" 1>&2
 fi
 
-if [ -f "${THEME_DIR}/_light.scss" ]; then
+if [ -f "$THEME_DIR/_light.scss" ]; then
   echo_ok "_light.scss exists"
 else
   echo_warn "_light.scss missing" 1>&2
 fi
 
-if [ -f "${THEME_DIR}/extras/_index.scss" ]; then
+if [ -f "$THEME_DIR/extras/_index.scss" ]; then
   echo_ok "extras/_index.scss exists"
 else
   echo_err "extras/_index.scss missing" 1>&2
   exit 1
 fi
 
-if [ -f "${THEME_DIR}/extras/hide-toggle.scss" ]; then
+if [ -f "$THEME_DIR/extras/hide-toggle.scss" ]; then
   echo_ok "extras/hide-toggle.scss exists"
 fi
 
-if [ -f "${THEME_DIR}/README.md" ]; then
+if [ -f "$THEME_DIR/README.md" ]; then
   echo_ok "README file exists"
 else
   echo_warn "README file missing"
@@ -157,12 +157,12 @@ fi
 
 echo "Verifying setup..."
 
-if grep -q '^@use "./themes";' ${THEME_DIR}/../custom.scss; then
+if grep -q '^@use "./themes";' "$THEME_DIR/../custom.scss"; then
   # Import already present in custom.scss
   echo_warn "Theme import line already present in custom.scss. Skipping..."
 else
   # Add `@use "./themes";` import to custom.scss
-  sed -ir 's#@use "./base.scss";#@use "./base.scss";\n@use "./themes";#' ${THEME_DIR}/../custom.scss
+  sed -ir 's#@use "./base.scss";#@use "./base.scss";\n@use "./themes";#' "$THEME_DIR/../custom.scss"
   echo_info "Added import line to custom.scss..."
 fi
 
