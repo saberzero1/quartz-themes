107fcf700e987a24dd38b86dc9ab9c85840a03e5
saberzero1 <github@emilebangma.com>
2025-08-25 06:29:14 +0200

    Themes

--- runner/scripts/extract.js
+++ runner/scripts/extract.js
@@ -1,6 +1,6 @@
 import { browser } from "@wdio/globals";
 import { extractionTargets } from "./config.js";
-import { writeFile, copyFile, writeFileSync, mkdirSync } from "fs";
+import { writeFile, copyFileSync, writeFileSync, mkdirSync } from "fs";
 import getManifestCollection from "../../extensions/manifest.mjs";
 import {
   isDarkTheme,
@@ -19,10 +19,15 @@ To load a theme by name: `app.customCss.setTheme("Abyssal");`
 
 let testingMode = false;
 //testingMode = true;
+const testingTheme = "Catppuccin";
 
 const manifestCollection = testingMode
-  ? getManifestCollection().slice(0, 10)
+  ? getManifestCollection().filter(
+      (m) => m.name.toLowerCase() === testingTheme.toLowerCase(),
+    )
   : getManifestCollection(); // Limit to first 10 themes for testing
+
+console.log(manifestCollection);
 
 // test/extract-styles.test.js
 describe("Quartz Theme Style Extraction", () => {
@@ -32,53 +37,24 @@ describe("Quartz Theme Style Extraction", () => {
         computedStyles = {};
         //app.customCss.setTheme(theme);
         const rules = targets.selectors;
-        /*
-      const file = app.vault.getAbstractFileByPath(value.file);
-      if (!file) {
-        throw new Error(
-          `File not found: ${value.file}. Ensure the file exists in the vault.`,
-        );
-      }
-      const leaf = app.workspace.getLeaf(false);
-      if (!leaf) {
-        throw new Error(
-          `Leaf not found for file: ${value.file}. Ensure the file is open in the workspace.`,
-        );
-      }
-      leaf.openFile(file, { active: true, eState: "tab" });
-      app.workspace.setActiveLeaf(leaf, { focus: true });
-      */
+
         await sleep(1000); // Wait for the file to open and styles to apply
-        //          for (const key in rules) {
         rules.forEach((rule) => {
           const containerSelector =
             "body > div.app-container > div.horizontal-main-container > div > div.workspace-split.mod-vertical.mod-root > div > div.workspace-tab-container > div > div > div.view-content > div.markdown-reading-view > div.markdown-preview-view";
           const containerElement =
-            //app.dom.appContainerEl.querySelector(containerSelector);
-            //app.workspace.activeLeaf.containerEl;
             app.workspace.activeLeaf.containerEl.querySelector(
               ".workspace-leaf-content > .view-content > .markdown-reading-view > .markdown-preview-view",
             );
           if (containerElement === null || containerElement === undefined)
             return;
 
-          //const style = app.dom.appContainerEl.getComputedStyle(element);
           const selectorKey =
             rule.pseudoElement === ""
               ? rule.obsidianSelector
               : `${rule.obsidianSelector}${rule.pseudoElement}`;
           const element =
             containerElement.querySelector(selectorKey) ?? containerElement;
-          /*
-        const style = element.computedStyleMap();
-        computedStyles[rule.quartzSelector] = {};
-        for (const prop of rule.properties) {
-          // Skip properties that are not set
-          computedStyles[rule.quartzSelector][prop] = style.get(prop)
-            ? style.get(prop).toString().trim()
-            : "unset";
-        }
-        */
           const style = getComputedStyle(element);
           computedStyles[rule.quartzSelector] = {};
           for (const prop of rule.properties) {
@@ -162,7 +138,7 @@ describe("Quartz Theme Style Extraction", () => {
         if (leftSidebar) {
           const leftSidebarStyle = getComputedStyle(leftSidebar);
           const borderStyle = getComputedStyle(borderElement);
-          computedStyles[`.left.sidebar`] = {
+          computedStyles[`.page > #quartz-body .left.sidebar`] = {
             "background-color": leftSidebarStyle.getPropertyValue(
               "background-color",
             )
@@ -179,6 +155,27 @@ describe("Quartz Theme Style Extraction", () => {
               : "inherit",
             "border-right-width": "1px",
           };
+          computedStyles[`.page > #quartz-body .sidebar.left:has(.explorer)`] =
+            {
+              "background-color": leftSidebarStyle.getPropertyValue(
+                "background-color",
+              )
+                ? leftSidebarStyle
+                    .getPropertyValue("background-color")
+                    .toString()
+                    .trim()
+                : "unset",
+            };
+          computedStyles[`.explorer .explorer-content`] = {
+            "background-color": leftSidebarStyle.getPropertyValue(
+              "background-color",
+            )
+              ? leftSidebarStyle
+                  .getPropertyValue("background-color")
+                  .toString()
+                  .trim()
+              : "unset",
+          };
         }
         const rightSidebar = document
           .getRootNode()
@@ -188,7 +185,7 @@ describe("Quartz Theme Style Extraction", () => {
         if (rightSidebar) {
           const rightSidebarStyle = getComputedStyle(rightSidebar);
           const borderStyle = getComputedStyle(borderElement);
-          computedStyles[`.right.sidebar`] = {
+          computedStyles[`.page > #quartz-body .right.sidebar`] = {
             "background-color": rightSidebarStyle.getPropertyValue(
               "background-color",
             )
@@ -209,7 +206,8 @@ describe("Quartz Theme Style Extraction", () => {
 
         // Generic
         computedStyles[`.page > #quartz-body div.center`] = {
-          "min-width": "unset",
+          "min-width": "calc(100% - 40px)",
+          "max-width": "calc(100% - 40px)",
           "padding-left": "10px",
           "padding-right": "10px",
         };
@@ -237,79 +235,7 @@ describe("Quartz Theme Style Extraction", () => {
     console.log(`Extracting styles for theme: ${theme}`);
 
     if (isDarkTheme(theme)) {
-      before(async () => {
-        console.log(`Setting up for theme: ${theme} in dark mode`);
-        /*
-        await browser.reloadObsidian(
-          "./runner/vault",
-          [
-            "./runner/vault/.obsidian/plugins/obsidian-style-settings",
-            "./runner/vault/.obsidian/plugins/obsidian-view-mode-by-frontmatter",
-          ],
-          `./runner/vault/.obsidian/theme/${theme}`,
-        );
-        await browser.executeObsidian(({ app }) => {
-          app.commands.executeCommandById("theme:use-dark");
-        });
-        await browser.executeObsidian(({ app }, theme) => {
-          app.customCss.setTheme(theme);
-        }, theme);
-        await browser.executeObsidian(({ app }) => {
-          app.plugins.enablePlugin("obsidian-style-settings");
-        });
-        await browser.executeObsidian(({ app }) => {
-          app.plugins.enablePlugin("obsidian-view-mode-by-frontmatter");
-        });
-        await browser.executeObsidian(({ app }) => {
-          app.plugins.enablePlugin("obsidian-hotkeys-for-specific-files");
-        });
-        await browser.executeObsidian(({ app }) => {
-          app.workspace.changeLayout(target);
-        });
-        await browser.executeObsidian(({ app }) => {
-          app.commands.executeCommandById(
-            `obsidian-hotkeys-for-specific-files:${file}`,
-          );
-        });
-        await browser.executeObsidian(({ app }, file) => {
-          const vaultFile = app.vault.getAbstractFileByPath(file);
-          if (vaultFile) {
-            app.workspace.getLeaf("tab").openFile(vaultFile);
-          }
-        });
-        await browser.reloadObsidian({ vault: "./runner/vault", theme: theme });
-
-        await browser.executeObsidian(({ app }) => {
-          app.commands.executeCommandById("theme:use-dark");
-        });
-        await browser.executeObsidian(({ app }) => {
-          app.plugins.enablePlugin("obsidian-style-settings");
-        });
-        await browser.executeObsidian(({ app }) => {
-          app.plugins.enablePlugin("obsidian-view-mode-by-frontmatter");
-        });
-        await browser.executeObsidian(({ app }) => {
-          app.plugins.enablePlugin("dataview");
-        });
-        */
-
-        //await sleep(1000);
-        console.log(`Setting up for theme: ${theme} in dark mode complete`);
-      });
-      /*
-      beforeEach(async () => {
-        const darkPage = browser.getObsidianPage();
-        await darkPage.loadWorkspaceLayout(darkKey);
-        await sleep(3000);
-        await darkPage.setTheme(theme);
-        await darkPage.enablePlugin("obsidian-style-settings");
-        await darkPage.enablePlugin("obsidian-view-mode-by-frontmatter");
-        await browser.executeObsidian(({ app }) => {
-          app.commands.executeCommandById("theme:use-dark");
-        });
-        await darkPage.openFile(`${darkKey}.md`);
-      });
-      */
+      before(async () => {});
       it(`should extract styles for theme: ${theme} in dark mode`, async () => {
         const darkPage = browser.getObsidianPage();
         await darkPage.resetVault();
@@ -328,44 +254,17 @@ describe("Quartz Theme Style Extraction", () => {
         await darkPage.openFile("general.md");
         await darkPage.openFile("general.md");
         darkResult.general = await getStyles(configuration.general, true);
-        //await darkPage.loadWorkspaceLayout("headings");
         await darkPage.openFile("headings.md");
         await darkPage.openFile("headings.md");
         darkResult.headings = await getStyles(configuration.headings);
-        //await darkPage.loadWorkspaceLayout("callouts");
         await darkPage.openFile("callouts.md");
         await darkPage.openFile("callouts.md");
         darkResult.callouts = await getStyles(configuration.callouts);
-        //await darkPage.loadWorkspaceLayout("integrations");
         await darkPage.openFile("integrations.md");
         await darkPage.openFile("integrations.md");
         darkResult.integrations = await getStyles(configuration.integrations);
         await sleep(500);
       });
-      /*
-        it(`should extract styles for theme: ${theme} in dark mode - headings`, async () => {
-          await browser.executeObsidian(({ app }) => {
-            app.workspace.changeLayout("headings");
-          });
-          darkHeadings = await getStyles(extractionTargets.headings);
-          sleep(1000);
-          console.log(
-            `Dark styles for headings layout extracted:`,
-            darkHeadings,
-          );
-        });
-        it(`should extract styles for theme: ${theme} in dark mode - callouts`, async () => {
-          await browser.executeObsidian(({ app }) => {
-            app.workspace.changeLayout("callouts");
-          });
-          darkCallouts = await getStyles(extractionTargets.callouts);
-          sleep(1000);
-          console.log(
-            `Dark styles for callouts layout extracted:`,
-            darkCallouts,
-          );
-        });
-        */
       after(async () => {
         const darkFileName = `./runner/results/${folder}/dark.json`;
         // Flatten the result object
@@ -394,86 +293,10 @@ describe("Quartz Theme Style Extraction", () => {
           );
         });
         console.log(`Dark styles saved to ${darkFileName}`);
-        //console.log(darkResult);
       });
     }
     if (isLightTheme(theme)) {
-      before(async () => {
-        console.log(`Setting up for theme: ${theme} in light mode`);
-        /*
-        await browser.reloadObsidian(
-          "./runner/vault",
-          [
-            "./runner/vault/.obsidian/plugins/obsidian-style-settings",
-            "./runner/vault/.obsidian/plugins/obsidian-view-mode-by-frontmatter",
-          ],
-          `./runner/vault/.obsidian/theme/${theme}`,
-        );
-        await browser.executeObsidian(({ app }) => {
-          app.commands.executeCommandById("theme:use-light");
-        });
-        await browser.executeObsidian(({ app }, theme) => {
-          app.customCss.setTheme(theme);
-        }, theme);
-        await browser.executeObsidian(({ app }) => {
-          app.plugins.enablePlugin("obsidian-style-settings");
-        });
-        await browser.executeObsidian(({ app }) => {
-          app.plugins.enablePlugin("obsidian-view-mode-by-frontmatter");
-        });
-        await browser.executeObsidian(({ app }) => {
-          app.plugins.enablePlugin("obsidian-hotkeys-for-specific-files");
-        });
-        await browser.executeObsidian(({ app }) => {
-          app.workspace.changeLayout(target);
-        });
-        await browser.executeObsidian(({ app }) => {
-          app.commands.executeCommandById(
-            `obsidian-hotkeys-for-specific-files:${file}`,
-          );
-        });
-        await browser.executeObsidian(({ app }, file) => {
-          const vaultFile = app.vault.getAbstractFileByPath(file);
-          if (vaultFile) {
-            app.workspace.getLeaf("tab").openFile(vaultFile);
-          }
-        });
-        await browser.reloadObsidian({ vault: "./runner/vault", theme: theme });
-
-        await browser.executeObsidian(({ app }) => {
-          app.commands.executeCommandById("theme:use-light");
-        });
-        await browser.executeObsidian(({ app }, theme) => {
-          app.customCss.setTheme(theme);
-        }, theme);
-        await browser.executeObsidian(({ app }) => {
-          app.plugins.enablePlugin("obsidian-style-settings");
-        });
-        await browser.executeObsidian(({ app }) => {
-          app.plugins.enablePlugin("obsidian-view-mode-by-frontmatter");
-        });
-        await browser.executeObsidian(({ app }) => {
-          app.plugins.enablePlugin("dataview");
-        });
-        */
-
-        //await sleep(1000);
-        console.log(`Setting up for theme: ${theme} in light mode complete`);
-      });
-      /*
-      beforeEach(async () => {
-        const lightPage = browser.getObsidianPage();
-        await lightPage.loadWorkspaceLayout(lightKey);
-        await sleep(3000);
-        await lightPage.setTheme(theme);
-        await lightPage.enablePlugin("obsidian-style-settings");
-        await lightPage.enablePlugin("obsidian-view-mode-by-frontmatter");
-        await browser.executeObsidian(({ app }) => {
-          app.commands.executeCommandById("theme:use-light");
-        });
-        await lightPage.openFile(`${lightKey}.md`);
-      });
-      */
+      before(async () => {});
       it(`should extract styles for theme: ${theme} in light mode`, async () => {
         const lightPage = browser.getObsidianPage();
         await lightPage.resetVault();
@@ -492,55 +315,17 @@ describe("Quartz Theme Style Extraction", () => {
         await lightPage.openFile("general.md");
         await lightPage.openFile("general.md");
         lightResult.general = await getStyles(configuration.general, true);
-        //await lightPage.loadWorkspaceLayout("headings");
         await lightPage.openFile("headings.md");
         await lightPage.openFile("headings.md");
         lightResult.headings = await getStyles(configuration.headings);
-        //await lightPage.loadWorkspaceLayout("callouts");
         await lightPage.openFile("callouts.md");
         await lightPage.openFile("callouts.md");
         lightResult.callouts = await getStyles(configuration.callouts);
-        //await lightPage.loadWorkspaceLayout("integrations");
         await lightPage.openFile("integrations.md");
         await lightPage.openFile("integrations.md");
         lightResult.integrations = await getStyles(configuration.integrations);
         await sleep(500);
       });
-      /*
-        it(`should extract styles for theme: ${theme} in light mode - general`, async () => {
-          await browser.executeObsidian(({ app }) => {
-            app.workspace.changeLayout("general");
-          });
-          lightGeneral = await getStyles(extractionTargets["general"]);
-          sleep(1000);
-          console.log(
-            `Light styles for general layout extracted:`,
-            lightGeneral,
-          );
-        });
-        it(`should extract styles for theme: ${theme} in light mode - headings`, async () => {
-          await browser.executeObsidian(({ app }) => {
-            app.workspace.changeLayout("headings");
-          });
-          lightHeadings = await getStyles(extractionTargets["headings"]);
-          sleep(1000);
-          console.log(
-            `Light styles for headings layout extracted:`,
-            lightHeadings,
-          );
-        });
-        it(`should extract styles for theme: ${theme} in light mode - callouts`, async () => {
-          await browser.executeObsidian(({ app }) => {
-            app.workspace.changeLayout("callouts");
-          });
-          lightCallouts = await getStyles(extractionTargets["callouts"]);
-          sleep(1000);
-          console.log(
-            `Light styles for callouts layout extracted:`,
-            lightCallouts,
-          );
-        });
-        */
       after(async () => {
         // Save the light result to a file
         const lightFileName = `./runner/results/${folder}/light.json`;
@@ -570,7 +355,6 @@ describe("Quartz Theme Style Extraction", () => {
           );
         });
         console.log(`Light styles saved to ${lightFileName}`);
-        //console.log(lightResult);
       });
     }
     return { lightResult, darkResult };
@@ -582,3 +366,10 @@ describe("Quartz Theme Style Extraction", () => {
     getStylesFromObsidian(manifest, extractionTargets);
   }
 });
+
+if (testingMode) {
+  copyFileSync(
+    `./runner/results/${sanitize(testingTheme)}/_index.scss`,
+    `./runner/quartz/quartz/styles/themes/_index.scss`,
+  );
+}
