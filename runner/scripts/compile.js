9401a9976ef4d4461dd7957256cddb7cf2042345
saberzero1 <github@emilebangma.com>
2025-11-08 00:14:17 +0100

    Updated mapping

--- runner/scripts/compile.js
+++ runner/scripts/compile.js
@@ -938,7 +938,7 @@ body {
         transition: opacity .2s;
       }
     }
-    div#quartz-root.page:not(:has(.center:hover, footer:hover)):has(.sidebar:hover) {
+    div#quartz-root.page:not(:has(.center:hover, footer:hover)):hover {
       .sidebar {
         opacity: 1 !important;
         transition: opacity .2s;
