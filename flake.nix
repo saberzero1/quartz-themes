a7a33923694e7e107465953c2d6cf3f3da83b51d
saberzero1 <github@emilebangma.com>
2025-12-04 09:45:04 +0100

    Separate darwin/linux packages

--- flake.nix
+++ flake.nix
@@ -22,65 +22,80 @@
           };
         };
 
+        darwinLibraries = (if pkgs.stdenv.isDarwin then with pkgs; [ ] else [ ]);
+
+        linuxLibraries = (
+          if pkgs.stdenv.isLinux then
+            with pkgs;
+            [
+              libdrm
+              alsa-lib
+              # Additional libraries for Chromium/Electron
+              udev
+              # X11 libraries for headless browser automation
+              xorg.libX11
+              xorg.libXcomposite
+              xorg.libXdamage
+              xorg.libXext
+              xorg.libXfixes
+              xorg.libXrandr
+              xorg.libxcb
+              xorg.libXcursor
+              xorg.libXi
+              xorg.libXrender
+              xorg.libXtst
+              xorg.libXScrnSaver
+              # GTK for Electron/Chromium-based applications
+              gtk3
+              gtk4
+              # Additional libraries for Chromium/Electron
+              libGL
+              # Graphics buffer management (required for Chromium)
+              libgbm
+              # Font configuration
+              fontconfig
+              freetype
+              # UUID generation (important for unique user-data-dir)
+              libuuid
+              # System utilities
+              systemd
+              # Wayland support
+              wayland
+              # Additional Electron dependencies
+              libnotify
+              libappindicator-gtk3
+              libdbusmenu
+            ]
+          else
+            [ ]
+        );
+
         # Libraries needed for dynamically linked binaries (e.g., chromedriver from webdriverio)
-        libraries = with pkgs; [
-          # Core libraries for dynamically linked executables
-          stdenv.cc.cc.lib
-          zlib
-          glib
-          nss
-          nspr
-          atk
-          cups
-          dbus
-          dbus.lib
-          expat
-          libdrm
-          libxkbcommon
-          pango
-          cairo
-          mesa
-          alsa-lib
-          at-spi2-atk
-          at-spi2-core
-          # X11 libraries for headless browser automation
-          xorg.libX11
-          xorg.libXcomposite
-          xorg.libXdamage
-          xorg.libXext
-          xorg.libXfixes
-          xorg.libXrandr
-          xorg.libxcb
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXrender
-          xorg.libXtst
-          xorg.libXScrnSaver
-          # GTK for Electron/Chromium-based applications
-          gtk3
-          gtk4
-          # Additional libraries for Chromium/Electron
-          libGL
-          udev
-          # Graphics buffer management (required for Chromium)
-          libgbm
-          # Font configuration
-          fontconfig
-          freetype
-          # UUID generation (important for unique user-data-dir)
-          libuuid
-          # System utilities
-          systemd
-          # Wayland support
-          wayland
-          # Additional Electron dependencies
-          libnotify
-          libappindicator-gtk3
-          libdbusmenu
-          # Networking
-          curl
-          wget
-        ];
+        libraries =
+          with pkgs;
+          [
+            # Core libraries for dynamically linked executables
+            stdenv.cc.cc.lib
+            zlib
+            glib
+            nss
+            nspr
+            atk
+            cups
+            dbus
+            dbus.lib
+            expat
+            libxkbcommon
+            pango
+            cairo
+            mesa
+            at-spi2-atk
+            at-spi2-core
+            # Networking
+            curl
+            wget
+          ]
+          ++ (if pkgs.stdenv.isDarwin then darwinLibraries else linuxLibraries);
 
         # Use nix-ld to provide dynamic linker for downloaded binaries
         nix-ld-libraries = pkgs.lib.makeLibraryPath libraries;
