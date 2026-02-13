1e5644fbbea98fc88a128f5944ab399420794dcb
saberzero1 <github@emilebangma.com>
2026-02-13 09:36:30 +0100

    chore: set cli as default for now

--- action_develop_branch.sh
+++ action_develop_branch.sh
@@ -7,6 +7,8 @@ set +x
 #
 # curl -s -S https://raw.githubusercontent.com/saberzero1/quartz-themes/develop/action.sh | bash -s -- <THEME_NAME>
 
+BRANCH="cli"
+
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
 GREEN='\033[0;32m'
@@ -29,17 +31,17 @@ QUARTZ_STYLES_DIR="quartz/styles"
 FINAL_THEME_DIR="./$QUARTZ_STYLES_DIR/$THEME_DIR"
 
 if [ -f "./$QUARTZ_STYLES_DIR/custom.scss" ]; then
-  echo_ok "Quartz root succesfully detected..."
-  FINAL_THEME_DIR="./$QUARTZ_STYLES_DIR/$THEME_DIR"
+	echo_ok "Quartz root succesfully detected..."
+	FINAL_THEME_DIR="./$QUARTZ_STYLES_DIR/$THEME_DIR"
 else
-  echo_warn "Quartz root not detected, checking if we are in the styles directory..."
-  if [ -f "./custom.scss" ]; then
-    echo_ok "Styles directory detected..."
-    FINAL_THEME_DIR="./$THEME_DIR"
-  else
-    echo_err "Cannot detect Quartz repository. Are you in the correct working directory?" 1>&2
-    exit 1
-  fi
+	echo_warn "Quartz root not detected, checking if we are in the styles directory..."
+	if [ -f "./custom.scss" ]; then
+		echo_ok "Styles directory detected..."
+		FINAL_THEME_DIR="./$THEME_DIR"
+	else
+		echo_err "Cannot detect Quartz repository. Are you in the correct working directory?" 1>&2
+		exit 1
+	fi
 fi
 
 echo -e "Input theme: ${BLUE}$*${NC}"
@@ -50,16 +52,16 @@ echo "Parsing input theme..."
 result=""
 
 for param in "$@"; do
-  if [[ -n "$result" ]]; then
-    result="$result-"
-  fi
+	if [[ -n "$result" ]]; then
+		result="$result-"
+	fi
 
-  result="$result$param"
+	result="$result$param"
 done
 
 if [ -z "$result" ]; then
-  echo_warn "No theme provided, defaulting to Tokyo Night..."
-  result="tokyo-night"
+	echo_warn "No theme provided, defaulting to Tokyo Night..."
+	result="tokyo-night"
 fi
 
 # Convert to lowercase
@@ -80,7 +82,7 @@ mkdir -p ${FINAL_THEME_DIR}
 echo "Fetching theme files..."
 
 # clone only the specified theme using sparse checkout to save bandwidth and time
-git clone -b develop -n --depth=1 --filter=tree:0 https://github.com/saberzero1/quartz-themes.git &>/dev/null
+git clone -b ${BRANCH} -n --depth=1 --filter=tree:0 https://github.com/saberzero1/quartz-themes.git &>/dev/null
 #git -C quartz-themes sparse-checkout set --no-cone /themes/${THEME} &>/dev/null
 git -C quartz-themes sparse-checkout set --no-cone /runner/results/${THEME} &>/dev/null
 git -C quartz-themes checkout &>/dev/null
@@ -93,8 +95,8 @@ rm -rf quartz-themes
 echo "Applying patches..."
 
 if grep -q -e "color-scheme: only dark;" -e "color-scheme: only light;" "$FINAL_THEME_DIR/_index.scss"; then
-  echo_warn "Single mode theme detected, applying patch..."
-  sed -i "/Component\.Darkmode\(\)/d" "quartz.layout.ts"
+	echo_warn "Single mode theme detected, applying patch..."
+	sed -i "/Component\.Darkmode\(\)/d" "quartz.layout.ts"
 fi
 
 echo "Verifying setup..."
@@ -102,12 +104,12 @@ echo "Verifying setup..."
 cd ${FINAL_THEME_DIR}
 
 if grep -q '^@use "./themes";' "../custom.scss"; then
-  # Import already present in custom.scss
-  echo_warn "Theme import line already present in custom.scss. Skipping..."
+	# Import already present in custom.scss
+	echo_warn "Theme import line already present in custom.scss. Skipping..."
 else
-  # Add `@use "./themes";` import to custom.scss
-  sed -ir 's#@use "./base.scss";#@use "./base.scss";\n@use "./themes";#' "../custom.scss"
-  echo_info "Added import line to custom.scss..."
+	# Add `@use "./themes";` import to custom.scss
+	sed -ir 's#@use "./base.scss";#@use "./base.scss";\n@use "./themes";#' "../custom.scss"
+	echo_info "Added import line to custom.scss..."
 fi
 
 echo_ok "Finished fetching and applying theme '${THEME}'."
